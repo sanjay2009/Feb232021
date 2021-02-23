b8fe14fd7192518ec88e739e4e42bc1966a6b2f2
sanjay2009 <ramesh.sanjay@gmail.com>
2021-02-23 15:43:34 +0530

    Updated whileLoop

--- src/main/java/reviseBasics/WhileStatement.java
+++ src/main/java/reviseBasics/WhileStatement.java
@@ -4,15 +4,25 @@ public class WhileStatement {
 
 	public static void main(String[] args) {
 		
-		int x = 1;
+//		int x = 1;
+//		
+//		while(x <= 10)
+//		{
+//			
+//			System.out.println(x);
+//			x++;
+//			
+//		}
 		
+		int x = 1;
+		int sum = 0;
 		while(x <= 10)
 		{
-			
-			System.out.println(x);
+			sum = sum + x;
 			x++;
-			
 		}
+		
+		System.out.println(sum);
 
 	}
 
