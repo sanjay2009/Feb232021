9e33a272a9e0ebe6ac61692057891759646e2db9
sanjay2009 <ramesh.sanjay@gmail.com>
2021-02-23 15:50:16 +0530

    Do While

---
+++ src/main/java/reviseBasics/DoWhile.java
@@ -0,0 +1,18 @@
+package reviseBasics;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		do
+		{
+			System.out.println(x);
+			x++;
+		}
+		
+		while(x <= 5);
+
+	}
+
+}
