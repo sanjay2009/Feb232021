567a468efe16e0c622e8e0fdfe487746ebd52a9e
sanjay2009 <ramesh.sanjay@gmail.com>
2021-02-23 16:15:09 +0530

    for loop one

---
+++ src/main/java/reviseBasics/For1.java
@@ -0,0 +1,18 @@
+package reviseBasics;
+
+public class For1 {
+	
+	//display message 5 times
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		String message = "I love Java";
+		for(int i = 1; i <= n; i++)
+		{
+			System.out.println(message);
+			
+		}
+	}
+
+}
