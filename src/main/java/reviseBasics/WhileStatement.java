b2a8d4e5327771e6670716c359d9aff849a75fc6
sanjay2009 <ramesh.sanjay@gmail.com>
2021-02-23 15:12:52 +0530

    WhileLoop

---
+++ src/main/java/reviseBasics/WhileStatement.java
@@ -0,0 +1,19 @@
+package reviseBasics;
+
+public class WhileStatement {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		
+		while(x <= 10)
+		{
+			
+			System.out.println(x);
+			x++;
+			
+		}
+
+	}
+
+}
